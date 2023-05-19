1803dddeca8e9797878e0301c01d548c53186349
Greg Johnston <gregmj.sco@gmail.com>
2023-05-19 15:33:50 +0900

    Practice examples from previous odin project added

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 console.log("Test");
+console.log("Test again");
+
+const myObject = {
+    property: 'Value!',
+    otherProperty: 23,
+    "obnoxious property": function(){
+        console.log("Called obnoxious property")
+    }
+};
+
+const personFactory = (name, age) => {
+    const sayHello = () => console.log('hello!');
+    return {name, age, sayHello};
+};
+
+const jeff = personFactory('jeff', 27);
+
+console.log(jeff.name);
+
+jeff.sayHello();
+
+

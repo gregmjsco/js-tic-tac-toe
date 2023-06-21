13231804c461daa63c1963fa638faae138014cd4
Greg Johnston <gregmj.sco@gmail.com>
2023-06-21 15:36:04 +0900

    Pull out code from connect four example

--- main.js
+++ main.js
@@ -2,51 +2,23 @@
 
 //Gameboard Module 
 const gameboard = (() => {
-    const rows = 3;
-    const columns = 3;
     const board = [
     ['1', '2', '3'],
     ['4', '5', '6'],
     ['7', '8', '9']
     ];
 
-    //for (let i = 0; i < rows; i++) {
-     //   board[i] = [];
-       /// for (let j = 0; j < columns; j++) {
-         // board[i].push(Cell());
-        //}
-      //}
-
     const getBoard = () => {
       console.log(board);
     };
 
-    const placeToken = (row, column, player) => {
+    const placeToken = (value, playerToken) => {
         //logic to place token
-        board[row][column] = player;
+        board[value] = placeToken;
     };
 
-    const printBoard = () => {
-        //logic to print board
-    };
-    return {getBoard, placeToken, printBoard};
+    return {getBoard, placeToken};
 })();
-
-
-function Cell(){
-  let value = 0;
-
-  const addToken = (player) => {
-    value = player;
-  };
-
-  const getValue = () => value;
-
-  return {
-    addToken,
-    getValue
-  };
-}
 
 const gameController = (() => {
 
@@ -54,5 +26,6 @@ const gameController = (() => {
 
 
 const playerFactory = (name, token) => {
+  
 
 }

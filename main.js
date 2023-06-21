6a22e0bbeebf00183aa2a0ced92ace6e86669e5f
Greg Johnston <gregmj.sco@gmail.com>
2023-06-21 18:39:03 +0900

    Add winning combinations to check for

--- main.js
+++ main.js
@@ -2,10 +2,37 @@
 
 //Gameboard Module 
 const gameboard = (() => {
-    const board = [
+
+  const playerFactory = (name, mark, ai, turn) => {
+    return { name, mark, ai, turn};
+  };
+
+  const player1 = playerFactory('player1', 'X', false, true);
+  const player2 = playerFactory('player2', 'O', false, false)
+
+  const board = [
     ['1', '2', '3'],
     ['4', '5', '6'],
     ['7', '8', '9']
+  ];
+
+  let winner = null; 
+
+  let turns = 0;
+
+  
+  
+
+     // Possible win combinations
+     const winCombos = [
+      [0,1,2],
+      [0,3,6],
+      [3,4,5],
+      [6,7,8],
+      [1,4,7],
+      [2,4,6],
+      [2,5,8],
+      [0,4,8]
     ];
 
     const getBoard = () => {
@@ -20,17 +47,7 @@ const gameboard = (() => {
     return {getBoard, placeToken};
 })();
 
-const gameController = (() => {
-
-})();
 
 
-const playerFactory = (name, token) => {
-  return {
-    name,
-    token
-  };
-};
 
-const player1 = playerFactory('Player 1', 'X');
-const player2 = playerFactory('Player 2', 'O');
+

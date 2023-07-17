c40a9ed8bb069f2477d0d0911b33e461f201853d
Greg Johnston <gregmj.sco@gmail.com>
2023-07-17 13:11:24 +0900

    add takeTurn to gameController

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ const playerFactory = (name, marker) => {
 
   placeMarker = cell => {
     cell = marker; 
+    return marker;
   };
 
   return {
@@ -39,12 +40,27 @@ const gameController = (() => {
 
   const board = gameBoard.board;
 
+  console.log(board[1]);
+  console.log(board);
   let activePlayer = player1;
   let winnerDeclared = false;
   let remainingSpots = 9; 
 
+  const takeTurn = () => {
+    let cell = prompt("Choose cell", 0);
 
-console.log(board);
+    board[cell] = activePlayer.marker;
+    console.log(board)
+    remainingSpots--; 
+  }
+  
+  if(remainingSpots > 0) {
+    takeTurn();
+  }
+  
+
+
+
 
 console.log(player1);
 console.log(player2);

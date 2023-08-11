f14daa491513ef335fca6563c38ab8ab2498f4f3
Greg Johnston <gregmj.sco@gmail.com>
2023-08-11 11:02:06 +0900

    Planned out correct psuedocode for checkWin and begin implementation

--- main.js
+++ main.js
@@ -66,11 +66,18 @@ const gameController = (() => {
   }
 
 
-  function arrayAlreadyHasArray(arr, testArr){
+  function checkForWin(playerArray){
+    for(let i = 0; i < winningCombinations.length; i++) {
+      winningCombinations[i].forEach(cell => {
+        if()
+      })
+    }
+  }
+  function arrayAlreadyHasArray(arr, playerArray){
         for(var i = 0; i<arr.length; i++){
             let checker = []
             for(var j = 0; j<arr[i].length; j++){
-                if(arr[i][j] === testArr[j]){
+                if(arr[i][j] === playerArray[j]){
                     checker.push(true)
                     
                 } else {
@@ -88,6 +95,19 @@ const gameController = (() => {
   
   const checkWin = () => {
     result = arrayAlreadyHasArray(winningCombinations, activePlayer.playerBoard);
+
+/*
+function checkForWin(whichPlayer){
+  - iterate over the winConditions array, each of which 
+    represents a single winCondition.
+    - For each winCondition, check if each value is present 
+      anywhere in the whichPlayer. 
+      - If it is not, drop out of the loop, this not a win
+      - If all the values of this winCondition are present in 
+        whichPlayer, then we can return a win!
+  - If we loop through all the winConditions and fall out the bottom, no win.
+}
+*/
     console.log(result);
     if (result == true) {
       console.log(`${activePlayer.name} WINS`);
